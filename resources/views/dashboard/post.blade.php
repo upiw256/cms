c128ed3ddc5262596ee59be65bc9372d42b73759
upiw256 <bilqimlb@gmail.com>
2023-08-08 09:43:42 +0700

    membuat table

--- resources/views/dashboard/post.blade.php
+++ resources/views/dashboard/post.blade.php
@@ -1,5 +1,42 @@
 @extends('dashboard.layout.content')
 
 @section('container')
-    <h1>Post</h1>
+<div class="p-3">
+    <div class="bg-white rounded shadow-inherit p-3 mb-3">
+        <div class="text-2xl font-extrabold font-sans">POST</div>
+    </div>
+    
+    <div class="bg-white rounded shadow-inherit p-3">
+        <div class="font-sans">
+            <button class="bg-blue-400 p-3 rounded-lg text-white hover:bg-blue-300"><i class="fa-regular fa-square-plus"></i> Postingan</button>
+            <div class="flex flex-col mt-2">
+                    <table class="text-center text-sm font-light">
+                        <thead
+                          class="border-b bg-neutral-50 font-medium dark:border-neutral-500 dark:text-neutral-800">
+                          <tr>
+                            <th scope="col" class=" px-6 py-4">#</th>
+                            <th scope="col" class=" px-6 py-4">Judul</th>
+                            <th scope="col" class=" px-6 py-4">isi</th>
+                            <th scope="col" class=" px-6 py-4">cover</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr class="border-b dark:border-neutral-500">
+                            <td class="whitespace-nowrap  px-6 py-4 font-medium">1</td>
+                            <td class="whitespace-nowrap  px-6 py-4">Mark</td>
+                            <td class="whitespace-nowrap  px-6 py-4">Otto</td>
+                            <td class="whitespace-nowrap  px-6 py-4">@mdo</td>
+                          </tr>
+                          <tr class="border-b dark:border-neutral-500">
+                            <td class="whitespace-nowrap  px-6 py-4 font-medium">2</td>
+                            <td class="whitespace-nowrap  px-6 py-4 ">Jacob</td>
+                            <td class="whitespace-nowrap  px-6 py-4">Thornton</td>
+                            <td class="whitespace-nowrap  px-6 py-4">@fat</td>
+                          </tr>
+                        </tbody>
+                    </table>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
